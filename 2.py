2f6fdf93770525355c2eb640a68312526670a48b
GONGATI NAVYA SRI <navyasrigongati@gmail.com>
2026-01-07 17:02:09 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,16 @@
+name="G.NAVYASRI"
+age=25
+height=5.4
+lst=[1,2,3,4,5,(1,2,3)]
+tuple=(1,2,3,"hello",[5,6])
+dict={1:'navya',2:'kavya',3:'divya'}
+set={5,3,4,2,1}
+a=None
+print("my name is:",name)
+print("i am ",age, "years old")
+print("my height is:",height)
+print(lst)
+print(dict)
+print(set)
+print(tuple)
+print(a)
