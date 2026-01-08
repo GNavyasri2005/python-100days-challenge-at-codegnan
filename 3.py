a3f3960e2e0ea4b3a35145a107b442cdfce38db0
GONGATI NAVYA SRI <navyasrigongati@gmail.com>
2026-01-08 16:51:52 +0530

    Add files via upload
    operators

---
+++ 3.py
@@ -0,0 +1,18 @@
+#simple calculator operations
+#variables and operators
+a=10
+b=20
+c=a+b
+print("sum of operators:",c)
+d=a-b
+print("subtraction of operators:",d)
+e=a*b
+print("multiplication of operators:",e)
+f=a/b
+print("division of operators:",f)
+g=a%b
+print("remainder of operators:",g)
+h=a//b
+print("floor division of operators:",h)
+i=a**b
+print("exponential of operators:",i)
